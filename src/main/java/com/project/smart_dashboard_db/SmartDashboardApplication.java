430e0f78ad27eab1e1040d9dca6b6bb4b62ad1f5
Mathieu Barral <mathieu.barral2@mail.dcu.ie>
2024-04-13 14:49:31 +0100

    Add embedded database connection parameters

--- src/main/java/com/project/smart_dashboard_db/SmartDashboardApplication.java
+++ src/main/java/com/project/smart_dashboard_db/SmartDashboardApplication.java
@@ -11,7 +11,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 @SpringBootApplication
 public class SmartDashboardApplication {
+    private static final String DATABASE_URL = "jdbc:mysql://ee417db.crxkzf89o3fh.eu-west-1.rds.amazonaws.com/smart_dashboard_db";
+    private static final String DATABASE_USERNAME = "EE417";
+    private static final String DATABASE_PASSWORD = "EE417_2024";
+    private static final String DATABASE_DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+
     public static void main(String[] args) {
+        System.setProperty("spring.datasource.url", DATABASE_URL);
+        System.setProperty("spring.datasource.username", DATABASE_USERNAME);
+        System.setProperty("spring.datasource.password", DATABASE_PASSWORD);
+        System.setProperty("spring.datasource.driver-class-name", DATABASE_DRIVER_CLASS_NAME);
+
         SpringApplication.run(SmartDashboardApplication.class, args);
     }
 }
